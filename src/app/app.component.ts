d2e5af27f6bce5d1495855ff61fba3155a538c5e
grvsharma2405 <grvsharma2405@gmail.com>
2020-04-09 22:49:10 +0530

    Version Beta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <!-- header --> 
+  <app-header></app-header>
+                        
+  <!-- routes get injected here -->
+  <router-outlet></router-outlet>
+
+  <!-- footer -->
+  <app-footer></app-footer>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'brij-pharma';
